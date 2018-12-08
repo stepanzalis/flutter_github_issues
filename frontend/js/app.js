1c20b48450e002db100b07c0a4faa212997f4aa0
Stepan Zalis <stepan.zalis@uhk.cz>
2018-12-08 22:59:50 +0200

    added firebase data and chart

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,16 +1,68 @@
+let chart = null;
+let datetime = [];
+let issueData = [];
 
-$(document).ready(function() {
+const OPEN_ISSUES = 0,
+    CLOSE_ISSUES = 1;
 
-    let ref = firebaseInit();
-    
-    ref.on('value', function (snapshot) {
-        snapshot.forEach(function (childSnapshot) {
-            var childData = childSnapshot.val();
-            console.log(childData.timestamp);
-        });
-    });
+$(document).ready(function () {
 
+    firebaseInit();
+    getData();
 });
+
+// Main function to parse dat from database
+async function getData() {
+
+    let data = await pullFromFirebase();
+    filterDateTime(data, 10);
+    filterIssuesData(data, OPEN_ISSUES);
+
+    console.table(datetime);
+
+    createChart();
+}
+
+
+/**
+ * Parse timestamp to human date time
+ * @param {data} data data All data from Database
+ * @param {day} days Number of days to be reversed to
+ */
+function filterDateTime(data, days) {
+
+    datetime = data.filter(function (iss) {
+        return new Date() - (1000 * 60 * 60 * 24 * days) < new Date(iss.timestamp);
+    }).map(issue => {
+        return new Date(issue.timestamp).toLocaleString();
+    });
+}
+
+/**
+ * Filter OPEN or CLOSE data depends on:
+ * @param {data} data All data from Database
+ * @param {*} variant OPEN or CLOSE issue
+ */
+function filterIssuesData(data, variant) {
+
+    issueData = data.filter(function (iss) {
+        return new Date() - (1000 * 60 * 60 * 24 * 10) < new Date(iss.timestamp);
+    }).map(issue => {
+        return variant === OPEN_ISSUES ? issues.open : issue.close;
+    });
+}
+
+
+
+// Get data from Firebase Database
+pullFromFirebase = () => {
+    return firebase.database().ref('issues').once('value').then(function (snapshot) {
+        const issues = Object.keys(snapshot.val()).map(function (key) {
+            return snapshot.val()[key];
+        });
+        return issues;
+    });
+}
 
 /** 
     Firebase config init
@@ -19,15 +71,47 @@ $(document).ready(function() {
 function firebaseInit() {
 
     var config = {
-            apiKey: "<API-KEY>",
-            authDomain: "flutter-github-issues.firebaseapp.com",
-            databaseURL: "https://flutter-github-issues.firebaseio.com",
-            projectId: "flutter-github-issues",
-            storageBucket: "flutter-github-issues.appspot.com",
-            messagingSenderId: "1082526570706"
-        };
+        apiKey: "AIzaSyBo_u2lsqYTT_FQbzaqFC02039wS6c8vhw",
+        authDomain: "flutter-github-issues.firebaseapp.com",
+        databaseURL: "https://flutter-github-issues.firebaseio.com",
+        projectId: "flutter-github-issues",
+        storageBucket: "flutter-github-issues.appspot.com",
+        messagingSenderId: "1082526570706"
+    };
 
-        firebase.initializeApp(config);
-
-        return firebase.database().ref('issues');
+    firebase.initializeApp(config);
 }
+
+// Draw a chart 
+function createChart() {
+
+    var ctx = $("#chart");
+
+    chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: datetime,
+            datasets: [{
+                label: '# of Open issues',
+                data: issueData,
+                backgroundColor: [
+                    'rgba(54, 162, 235, 0.2)',
+                ],
+                borderColor: [
+                    '#45B3F1'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
+
