217aacf536c69244f5e3b5ed359cc00e7f398cf6
Stepan Zalis <stepan.zalis@uhk.cz>
2018-12-19 11:52:26 +0200

    added preloader

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,6 +1,7 @@
 
 var instance_tabs = null;
 
+// CHARTS
 let chartToday = null;
 let chartMonth = null;
 let chartTotal = null;
@@ -8,7 +9,7 @@ let chartTotal = null;
 let data = null;
 
 // CONSTANTS
-const TIME = 0; 
+const TIME = 0;
 const DATE = 1;
 
 $(document).ready(function () {
@@ -25,15 +26,15 @@ $(document).ready(function () {
 function tab_callback() {
 
     switch (instance_tabs.index) {
-        case 0:
+        case 0: // TODAY
             notifyToday();
             break;
-        case 1:
+        case 1: // MONTH
             if (chartMonth == null) chartMonth = createChart("#month");
             let month = otherData(30, 6);
             notifyChart(chartMonth, month, DATE);
             break;
-        case 2:
+        case 2: // ALL
             if (chartTotal == null) chartTotal = createChart("#total");
             let days = data.length / 24;
             let total = otherData(days, 12);
@@ -53,127 +54,132 @@ function notifyToday() {
 // Main function to parse dat from database
 async function getData() {
     if (data == null) data = await pullFromFirebase();
+    hidePreloader();
     notifyToday();
 }
 
-// Get today data
-function todayData() {
-    return data.filter(function (issue) {
-        let now = new Date();
-        let iss = new Date(issue.timestamp);
-        return now.getDate() === iss.getDate() && now.getMonth() === iss.getMonth();
-    });
+function hidePreloader() {
+    document.getElementById("preloader").style.visibility = 'hidden';
 }
 
-/**
- * @param days Number of days to be filter
- * @param skip Filter offset 
- */
-function otherData(days, skip) {
-    return data.filter(function (issue) {
-        return new Date() - (1000 * 60 * 60 * 24 * days) <= new Date(issue.timestamp);
-    }).filter(function (_, index, _) {
-        if (index % skip == 0) return this;
-    });
-}
-
-// Get data from Firebase Database
-pullFromFirebase = () => {
-    return firebase.database().ref('issues').once('value').then(function (snapshot) {
-        const issues = Object.keys(snapshot.val()).map(function (key) {
-            return snapshot.val()[key];
+    // Get today data
+    function todayData() {
+        return data.filter(function (issue) {
+            let now = new Date();
+            let iss = new Date(issue.timestamp);
+            return now.getDate() === iss.getDate() && now.getMonth() === iss.getMonth();
         });
-        return issues;
-    });
-}
+    }
 
-/*
-*  Firebase config init
-*  @return Firebase Database reference of all issues
-*/
-function firebaseInit() {
+    /**
+     * @param days Number of days to be filter
+     * @param skip Filter offset 
+     */
+    function otherData(days, skip) {
+        return data.filter(function (issue) {
+            return new Date() - (1000 * 60 * 60 * 24 * days) <= new Date(issue.timestamp);
+        }).filter(function (_, index, _) {
+            if (index % skip == 0) return this;
+        });
+    }
 
-    var config = {
-        apiKey: "AIzaSyBo_u2lsqYTT_FQbzaqFC02039wS6c8vhw",
-        authDomain: "flutter-github-issues.firebaseapp.com",
-        databaseURL: "https://flutter-github-issues.firebaseio.com",
-        projectId: "flutter-github-issues",
-        storageBucket: "flutter-github-issues.appspot.com",
-        messagingSenderId: "1082526570706"
-    };
+    // Get data from Firebase Database
+    pullFromFirebase = () => {
+        return firebase.database().ref('issues').once('value').then(function (snapshot) {
+            const issues = Object.keys(snapshot.val()).map(function (key) {
+                return snapshot.val()[key];
+            });
+            return issues;
+        });
+    }
 
-    firebase.initializeApp(config);
-}
+    /*
+    *  Firebase config init
+    *  @return Firebase Database reference of all issues
+    */
+    function firebaseInit() {
 
-// Draw a chart 
-function createChart(chartId) {
+        var config = {
+            apiKey: "AIzaSyBo_u2lsqYTT_FQbzaqFC02039wS6c8vhw",
+            authDomain: "flutter-github-issues.firebaseapp.com",
+            databaseURL: "https://flutter-github-issues.firebaseio.com",
+            projectId: "flutter-github-issues",
+            storageBucket: "flutter-github-issues.appspot.com",
+            messagingSenderId: "1082526570706"
+        };
 
-    var ctx = $(chartId);
+        firebase.initializeApp(config);
+    }
 
-    return new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: [],
-            datasets: [{
-                label: '# of OPENED issues',
-                data: [],
-                backgroundColor: [
-                    'rgba(54, 162, 235, 0.2)',
-                ],
-                borderColor: [
-                    '#45B3F1'
-                ],
-                borderWidth: 1
+    // Draw a chart 
+    function createChart(chartId) {
+
+        var ctx = $(chartId);
+
+        return new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: [],
+                datasets: [{
+                    label: '# of OPENED issues',
+                    data: [],
+                    backgroundColor: [
+                        'rgba(54, 162, 235, 0.2)',
+                    ],
+                    borderColor: [
+                        '#45B3F1'
+                    ],
+                    borderWidth: 1
+                },
+                {
+                    label: '# of CLOSED issues',
+                    data: [],
+                    hidden: true,
+                    backgroundColor: [
+                        '#EF9A9A',
+                    ],
+                    borderColor: [
+                        '#D32F2F'
+                    ],
+                    borderWidth: 1
+                }
+                ]
             },
-            {
-                label: '# of CLOSED issues',
-                data: [],
-                hidden: true,
-                backgroundColor: [
-                    '#EF9A9A',
-                ],
-                borderColor: [
-                    '#D32F2F'
-                ],
-                borderWidth: 1
-            }
-            ]
-        },
-        options: {
-            responsive: true,
-            maintainAspectRatio: false,
-            scales: {
-                yAxes: [{
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Number of issues'
-                    },
-                    ticks: {
-                        major: {
-                            fontStyle: 'bold',
-                            fontColor: '#FF0000'
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    yAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Number of issues'
+                        },
+                        ticks: {
+                            major: {
+                                fontStyle: 'bold',
+                                fontColor: '#FF0000'
+                            }
                         }
-                    }
-                }]
+                    }]
+                }
             }
-        }
-    });
-}
+        });
+    }
 
-/**
- * @param chart Id of chart to bind data
- * @param dataset Data
- * @param time show only TIME or DATE (TIME used in today's stats)
- */
-function notifyChart(chart, dataset, time) {
+    /**
+     * @param chart Id of chart to bind data
+     * @param dataset Data
+     * @param time show only TIME or DATE (TIME used in today's stats)
+     */
+    function notifyChart(chart, dataset, time) {
 
-    let open = dataset.map(issue => issue.open);
-    let closed = dataset.map(issue => issue.close);
-    let datetime = dataset.map(issue => time === TIME ? new Date(issue.timestamp).toLocaleTimeString() : new Date(issue.timestamp).toLocaleDateString());
+        let open = dataset.map(issue => issue.open);
+        let closed = dataset.map(issue => issue.close);
+        let datetime = dataset.map(issue => time === TIME ? new Date(issue.timestamp).toLocaleTimeString() : new Date(issue.timestamp).toLocaleDateString());
 
-    chart.data.datasets[0].data = open;
-    chart.data.datasets[1].data = closed;
-    chart.data.labels = datetime;
-    chart.update();
-}
+        chart.data.datasets[0].data = open;
+        chart.data.datasets[1].data = closed;
+        chart.data.labels = datetime;
+        chart.update();
+    }
 
